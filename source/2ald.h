9dd5edd95589f69fca96c062d97c27c728e74c6c
Stray1144 <stray1144@gmail.com>
2026-04-05 20:56:18 -0500

    2ald: unused things removal

--- source/2ald.h
+++ source/2ald.h
@@ -19,21 +19,9 @@ typedef enum output_format_kind_e {
 typedef struct settings_s {
         char *output_file;
         output_format_kind_t output_format;
-        // char *output_format; // "cxreo" or "flat"
 
         bool logger_timestamp;
 } settings_t;
-
-typedef struct merger_s {
-        buffer_t file_list;
-        reo_size_t old_sizes[REO_SECTION_COUNT];
-} merger_t;
-
-void merger_clear(merger_t *merger);
-bool merger_init(merger_t *merger);
-
-bool merger_upload(merger_t *merger, const char *path);
-bool merge(merger_t *merger, reo_file_t *product);
 
 typedef uint64_t arcx_address_t;
 
@@ -78,23 +66,6 @@ typedef struct context_s {
         
         archiver_t archiver;
 } context_t;
-
-// typedef struct translation_unit_s {
-//         char *name;
-//         buffer_t code_section; // buffer<uint8_t>
-//         buffer_t data_section; // buffer<uint8_t>
-//         reo_size_t block;
-//         buffer_t symbols;      // buffer<symbol_t>
-//         buffer_t relocations;  // buffer<symbol_t>
-//         uint32_t actual_section;
-// } translation_unit_t;
-//
-// void translation_unit_write(context_t *context, translation_unit_t *translation_unit);
-//
-// bool translation_unit_init(translation_unit_t *translation_unit, char *name);
-// void translation_unit_clear(translation_unit_t *translation_unit);
-//
-// bool translation_unit_assemble(context_t *context, translation_unit_t *translation_unit, char *source_path);
 
 #define SYSTEM_LOGGER(name) system_##name(context_t *context, char *prefix, char *format, ...)
 
